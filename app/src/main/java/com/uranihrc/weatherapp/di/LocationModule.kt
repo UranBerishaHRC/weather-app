61c34f966f43872f6b084a08e4d057ab0102dc28
UranBerishaHRC <uran.berisha@qrios.se>
2022-08-15 11:58:27 +0200

    - impl domain layer

---
+++ app/src/main/java/com/uranihrc/weatherapp/di/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.uranihrc.weatherapp.di
+
+import com.uranihrc.weatherapp.data.location.DefaultLocationTracker
+import com.uranihrc.weatherapp.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/com/uranihrc/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.uranihrc.weatherapp.presentation
+
+import com.uranihrc.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
